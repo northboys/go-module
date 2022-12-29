af95398c7683b331b8cbd70d32f68184422939ad
Ach Izalul Haq <izalul@mit.id>
2022-12-29 14:19:46 +0700

    first commit

--- keepo/keepo.go
+++ keepo/keepo.go
@@ -6,7 +6,41 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
+
+type Branch struct {
+	Name   string `json:"name"`
+	Commit struct {
+		ID             string      `json:"id"`
+		ShortID        string      `json:"short_id"`
+		CreatedAt      time.Time   `json:"created_at"`
+		ParentIds      interface{} `json:"parent_ids"`
+		Title          string      `json:"title"`
+		Message        string      `json:"message"`
+		AuthorName     string      `json:"author_name"`
+		AuthorEmail    string      `json:"author_email"`
+		AuthoredDate   time.Time   `json:"authored_date"`
+		CommitterName  string      `json:"committer_name"`
+		CommitterEmail string      `json:"committer_email"`
+		CommittedDate  time.Time   `json:"committed_date"`
+		Trailers       interface{} `json:"trailers"`
+		WebURL         string      `json:"web_url"`
+	} `json:"commit"`
+	Merged             bool   `json:"merged"`
+	Protected          bool   `json:"protected"`
+	DevelopersCanPush  bool   `json:"developers_can_push"`
+	DevelopersCanMerge bool   `json:"developers_can_merge"`
+	CanPush            bool   `json:"can_push"`
+	Default            bool   `json:"default"`
+	WebURL             string `json:"web_url"`
+}
+
+type Results struct {
+	Status       string   `json:"status"`
+	TotalResults int      `json:"totalResults"`
+	Branches     []Branch `json:"branches"`
+}
 
 type Client struct {
 	http     *http.Client
